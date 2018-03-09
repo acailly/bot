7b85fd4a839fde389ebd59abbf568beda0483b43
acailly <ylliac14@gmail.com>
2018-03-09 13:44:43 +0100

    :hammer: Show a warning when no or empty config file is found

--- bot/src/loadConfig.js
+++ bot/src/loadConfig.js
@@ -1,12 +1,20 @@
 const cosmiconfig = require("cosmiconfig");
 const path = require("path");
+const chalk = require("chalk");
 
 const rootDirectory = process.cwd() + "/";
 const configPath = path.join(rootDirectory, "config.yaml");
 
 function loadConfig() {
-  const configLoader = cosmiconfig("bot", { configPath, sync: true });
-  return configLoader.load().config;
+  try {
+    const configLoader = cosmiconfig("bot", { configPath, sync: true });
+    return configLoader.load().config;
+  } catch (e) {
+    const warning = chalk.bgYellow(chalk.black(" WARNING "));
+    const configFileName = chalk.yellow("config.yaml");
+    console.log(`${warning} File ${configFileName} was not found or was empty`);
+    return {};
+  }
 }
 
 module.exports = loadConfig;
