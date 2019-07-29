a815c95b0bbc77794699f38e67fb74ff88427ff2
Antoine CAILLY <ylliac14@gmail.com>
2019-07-29 21:53:12 +0200

    Update install_dependencies.js

--- bot/scripts/install_dependencies.js
+++ bot/scripts/install_dependencies.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const execSync = require("child_process").execSync;
 
 try {
-  if (!fs.exists("./node_modules")) {
+  if (!fs.existsSync("./node_modules")) {
     console.log("Installing dependencies...");
     execSync("yarn");
     console.log("Installing dependencies... done");
